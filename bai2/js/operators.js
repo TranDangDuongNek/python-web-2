e7a7d206fc7a00fc5d9154de585184a85a69e99f
TranDangDuongNek <anhti1987vn@gmail.com>
2024-10-27 11:29:02 +0700

    first commit

---
+++ bai2/js/operators.js
@@ -0,0 +1,21 @@
+// 1. toán tử sớ học 
+
+var a =12 ** 2; // luỹ thừa
+console.log(a) // 144
+// tiền tố (++a)
+
+console.long(++a) //145
+console.long(a) //145
+// hậu tố (a++)
+
+console.long(a++) // 144
+console.long(a) //145
+
+console.log(12%3) // chai lấy phna62 dư
+consolelog(13/2)// 6,5
+
+//2 toàn tử gần
+var x =10
+x += 100; // x = x+100
+
+//3 toán tử so sánh
---
+++ bai2/js/variable_builtin.js
@@ -0,0 +1,54 @@
+// input, output -------------------------
+// alert("This is notification!");
+// const full_name = prompt("Nhap ten cua ban");
+// console.log(typeof full_name);
+// const is_human = confirm("Are you human?");
+// console.log(is_human);
+// variable (scope) ------------------------
+// *** var
+// for (var index = 0; index < 10; index++) {
+//   console.log(index);
+// }
+
+// console.log(index); // 10
+// ***let
+// for (let index = 0; index < 10; index++) {
+//   console.log(index);
+// }
+
+// console.log(index); // error (is not defined)
+// *** const
+const object = { name: "abc", age: 15 };
+// object += {} // error (is constant var)
+// object.name = "xyz";
+// console.table(object);
+
+// builtin (set timeout, set interval)---------------------------
+// *** set timeout ***
+// way 1
+// setTimeout(() => {
+//   // arrow function
+//   document.write("<h1>Hello</h1>");
+// }, 1000);
+
+// way 2
+// function say_hello() {
+//   console.warn("Hello friend!");
+// }
+// setTimeout(say_hello, 1000);
+
+// set interval--------------------------------
+// const id = setInterval(() => {
+//     document.getElementsByTagName("body")[0].innerHTML +="<h1>hello</h1>"
+// }, 1000);
+
+// const id2 = setInterval(() => {
+//     document.getElementsByTagName("body")[0].innerHTML +="<h1>bye</h1>"
+// }, 1000);
+// // sau 5 lần dừng lại
+// setTimeout(() => {
+//     clearInterval(id);
+//  setTimeout(() => {
+//      clearInterval(id2);
+//  }, 5000);
+// }, 6000);
